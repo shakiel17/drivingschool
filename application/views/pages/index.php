50a4cfbe3d3df9d3d3e1f11017657d9350253583
shakiel17 <hisoka.aboy@gmail.com>
2024-03-07 09:57:52 +0800

    Update index.php

---
+++ application/views/pages/index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en" class="" style="height: auto;">
+<style>
+  #header{
+    height:70vh;
+    width:calc(100%);
+    position:relative;
+    top:-1em;
+  }
+  #header:before{
+    content:"";
+    position:absolute;
+    height:calc(100%);
+    width:calc(100%);
+    background-image:url(<?= validate_image($_settings->info("cover")) ?>);
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-position: center bottom;
+  }
+  #header>div{
+    position:absolute;
+    height:calc(100%);
+    width:calc(100%);
+    z-index:2;
+  }
+
+  #top-Nav a.nav-link.active {
+      color: #001f3f;
+      font-weight: 900;
+      position: relative;
+  }
+  #top-Nav a.nav-link.active:after {
+    content: "";
+    position: absolute;
+    border-bottom: 2px solid #001f3f;
+    width: 33.33%;
+    left: 33.33%;
+    bottom: 0;
+  }
+</style>
+<head>
+  <style>
+    :root{
+      --bg-img:url('<?php echo validate_image($_settings->info('cover')) ?>');
+    }
+  </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $_settings->info('title') != false ? $_settings->info('title').' | ' : '' ?><?php echo $_settings->info('name') ?></title>
+    <link rel="icon" href="<?php echo validate_image($_settings->info('logo')) ?>" />
+    <!-- Google Font: Source Sans Pro -->
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback"> -->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url ?>plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="<?php echo base_url ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+      <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+   <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="<?php echo base_url ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="<?php echo base_url ?>plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url ?>dist/css/adminlte.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>dist/css/custom.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>assets/css/styles.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="<?php echo base_url ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="<?php echo base_url ?>plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="<?php echo base_url ?>plugins/summernote/summernote-bs4.min.css">
+     <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+    <style type="text/css">/* Chart.js */
+      @keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}
+    </style>
+
+     <!-- jQuery -->
+    <script src="<?php echo base_url ?>plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?php echo base_url ?>plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- SweetAlert2 -->
+    <script src="<?php echo base_url ?>plugins/sweetalert2/sweetalert2.min.js"></script>
+    <!-- Toastr -->
+    <script src="<?php echo base_url ?>plugins/toastr/toastr.min.js"></script>
+    <script>
+        var _base_url_ = '<?php echo base_url ?>';
+    </script>
+    <script src="<?php echo base_url ?>dist/js/script.js"></script>
+    <style>
+    #main-header{
+        position:relative;
+        background: rgb(0,0,0)!important;
+        background: radial-gradient(circle, rgba(0,0,0,0.48503151260504207) 22%, rgba(0,0,0,0.39539565826330536) 49%, rgba(0,212,255,0) 100%)!important;
+    }
+    #main-header:before{
+        content:"";
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        background-image:url(<?php echo base_url.$_settings->info('cover') ?>);
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: drop-shadow(0px 7px 6px black);
+        z-index:-1;
+    }
+
+ </style>
+  </head>
+  <body class="layout-top-nav layout-fixed layout-navbar-fixed" style="height: auto;">
+    <div class="wrapper">
+     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
+     <?php require_once('inc/topBarNav.php') ?>
+     <?php if($_settings->chk_flashdata('success')): ?>
+      <script>
+        alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
+      </script>
+      <?php endif;?>    
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper pt-5" style="min-height: 567.854px;">
+        <?php if($page == "home" || $page == "about_us"): ?>
+          <div id="header" class="shadow mb-4">
+              <div class="d-flex justify-content-center h-100 w-100 align-items-center flex-column px-3">
+                  <h1 class="w-100 text-center site-title"><?php echo $_settings->info('name') ?></h1>
+                  <a href="./?page=enrollment" class="btn btn-lg btn-light rounded-pill w-25" id="enrollment"><b>Enroll Now</b></a>
+              </div>
+          </div>
+        <?php endif; ?>
+        <!-- Main content -->
+        <section class="content ">
+          <div class="container">
+            <?php 
+              if(!file_exists($page.".php") && !is_dir($page)){
+                  include '404.html';
+              }else{
+                if(is_dir($page))
+                  include $page.'/index.php';
+                else
+                  include $page.'.php';
+
+              }
+            ?>
+          </div>
+        </section>
+        <!-- /.content -->
+        <footer class="main-footer text-sm">
+  <div class="container">
+        <strong>Copyright © <?php echo date('Y') ?>. 
+        <!-- <a href=""></a> -->
+        </strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+          <b><?php echo $_settings->info('short_name') ?> (by: <a href="mailto:oretnom23@gmail.com" target="blank">oretnom23</a> )</b> v1.0
+        </div>
+      </div>
+      </footer>
+    </div>
+    <!-- ./wrapper -->
+<div id="libraries">
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+      $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="<?php echo base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- ChartJS -->
+    <script src="<?php echo base_url ?>plugins/chart.js/Chart.min.js"></script>
+    <!-- Sparkline -->
+    <script src="<?php echo base_url ?>plugins/sparklines/sparkline.js"></script>
+    <!-- Select2 -->
+    <script src="<?php echo base_url ?>plugins/select2/js/select2.full.min.js"></script>
+    <!-- JQVMap -->
+    <script src="<?php echo base_url ?>plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="<?php echo base_url ?>plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="<?php echo base_url ?>plugins/jquery-knob/jquery.knob.min.js"></script>
+    <!-- daterangepicker -->
+    <script src="<?php echo base_url ?>plugins/moment/moment.min.js"></script>
+    <script src="<?php echo base_url ?>plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="<?php echo base_url ?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Summernote -->
+    <script src="<?php echo base_url ?>plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="<?php echo base_url ?>plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?php echo base_url ?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?php echo base_url ?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <!-- overlayScrollbars -->
+    <!-- <script src="<?php echo base_url ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url ?>dist/js/adminlte.js"></script>
+  </div>   
+    <div class="daterangepicker ltr show-ranges opensright">
+      <div class="ranges">
+        <ul>
+          <li data-range-key="Today">Today</li>
+          <li data-range-key="Yesterday">Yesterday</li>
+          <li data-range-key="Last 7 Days">Last 7 Days</li>
+          <li data-range-key="Last 30 Days">Last 30 Days</li>
+          <li data-range-key="This Month">This Month</li>
+          <li data-range-key="Last Month">Last Month</li>
+          <li data-range-key="Custom Range">Custom Range</li>
+        </ul>
+      </div>
+      <div class="drp-calendar left">
+        <div class="calendar-table"></div>
+        <div class="calendar-time" style="display: none;"></div>
+      </div>
+      <div class="drp-calendar right">
+        <div class="calendar-table"></div>
+        <div class="calendar-time" style="display: none;"></div>
+      </div>
+      <div class="drp-buttons"><span class="drp-selected"></span><button class="cancelBtn btn btn-sm btn-default" type="button">Cancel</button><button class="applyBtn btn btn-sm btn-primary" disabled="disabled" type="button">Apply</button> </div>
+    </div>
+    <div class="jqvmap-label" style="display: none; left: 1093.83px; top: 394.361px;">Idaho</div>
+    </body>
+</html>
